1caa45a90316ceec70122e5ebb7d73f8e8d1c47f
Zoe Schmitt <zoeschmitt@hotmail.com>
2021-05-26 12:26:13 -0500

    components

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
@@ -6,22 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
-  votes: number;
-  title: string;
-  link: string;
+  @HostBinding('attr.class') cssClass = 'row';
+  @Input() article!: Article;
 
-  constructor() { 
-    this.title = 'Angular';
-    this.link = 'http://angular.io'
-    this.votes = 10;
+  constructor() {
   }
 
-  voteUp() {
-    this.votes += 1;
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
   }
 
-  voteDown() {
-    this.votes -= 1;
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
   }
 
   ngOnInit(): void {
